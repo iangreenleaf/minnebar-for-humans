2bc10a765919880c741d9c2f3a4b48086d9d7b8c
Themi Megas <Tcm4760@gmail.com>
2026-03-24 18:28:50 -0500

    Now extracts categories and session names and saves them to a file. Incomplete test state still.

--- src/scrape_minnebar.js
+++ src/scrape_minnebar.js
@@ -1,17 +1,40 @@
 import * as cheerio from 'cheerio';
 import axios from 'axios';
 import {scrape_result} from './test_data.js';
+import fs from 'node:fs';
 
+// Constants
 const allSessions = "https://sessions.minnestar.org/events/46/sessions";
-const outFile = "./raw_sessions.json";
+const minnebarBase = "https://sessions.minnestar.org";
+const dataFile = "./src/raw_sessions.json";
 
+// Handles fetching all sessions page
 // const allSessionReq = await axios.get(allSessions);
 // const allSessionCheerio = cheerio.load(allSessionReq.data);
-const allSessionCheerio = cheerio.load(scrape_result);
+const $allSession = cheerio.load(scrape_result);
 
-const sessionAElements = allSessionCheerio("a[href^='/sessions/']:not([class])");
-const sessionDetails = []
+// Reads all data already scraped in the past
+const readFile = fs.readFileSync(dataFile);
+const sessionDetails = JSON.parse(readFile);
+
+// Scrapes pages detected as new
+const sessionAElements = $allSession("a[href^='/sessions/']:not([class])");
 for (const sessionA of sessionAElements) {
-  sessionDetails.push({"rel_link": sessionA.attribs['href']});
+  const sessionUrl = `${minnebarBase}${sessionA.attribs['href']}`;
+  const sessionPageReq = await axios.get(sessionUrl);
+  const $sessionPage = cheerio.load(sessionPageReq.data);
+
+  const title = $sessionPage("h1.page-title").text();
+
+  const tags = [];
+  for (const tagElement of $sessionPage("ul.tags > li")) {
+    tags.push($sessionPage(tagElement).text());
+  }
+
+  sessionDetails.push({"url": sessionUrl, "title": title, "tags": tags})
+
+  break;
 }
-console.log(sessionDetails);
+
+// Saves updated sessions list
+fs.writeFileSync(dataFile, JSON.stringify(sessionDetails));

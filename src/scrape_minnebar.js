c14d071e57468a4a8ba7eb667ed9743e97ceff9c
Ian Young <ian@iangreenleaf.com>
2026-03-24 21:57:52 -0500

    Remove try/catch and throw an ugly error

--- src/scrape_minnebar.js
+++ src/scrape_minnebar.js
@@ -18,35 +18,32 @@ const sessionDetails = JSON.parse(readFile).sessions;
 
 // Scrapes pages detected as new
 const sessionAElements = $allSession("a[href^='/sessions/']:not([class])");
-try {
-  for (const sessionA of sessionAElements) {
-    const sessionUrl = `${baseURL}${sessionA.attribs['href']}`;
 
-    // If page is already scraped, skip it
-    if (sessionDetails.some(session => session.url === sessionUrl)) {
-      console.log(`Skipping ${sessionUrl}: already fetched`);
-      continue;
-    }
+for (const sessionA of sessionAElements) {
+  const sessionUrl = `${baseURL}${sessionA.attribs['href']}`;
 
-    await new Promise(resolve => setTimeout(resolve, requestDelayTime));
-    console.log(`Fetching ${sessionUrl}...`);
-    const sessionPageReq = await axios.get(sessionUrl);
-    const $sessionPage = cheerio.load(sessionPageReq.data);
-
-    const title = $sessionPage("h1.page-title").text();
-
-    const tags = [];
-    for (const tagElement of $sessionPage("ul.tags > li")) {
-      tags.push($sessionPage(tagElement).text());
-    }
-
-    $sessionPage(".session_description .tags").remove();
-    const description = $sessionPage(".session_description").text();
-
-    sessionDetails.push({"url": sessionUrl, title, description, tags});
+  // If page is already scraped, skip it
+  if (sessionDetails.some(session => session.url === sessionUrl)) {
+    console.log(`Skipping ${sessionUrl}: already fetched`);
+    continue;
   }
-} catch {
-  console.log("Failed to fetch all new sessions!");
+
+  await new Promise(resolve => setTimeout(resolve, requestDelayTime));
+  console.log(`Fetching ${sessionUrl}...`);
+  const sessionPageReq = await axios.get(sessionUrl);
+  const $sessionPage = cheerio.load(sessionPageReq.data);
+
+  const title = $sessionPage("h1.page-title").text();
+
+  const tags = [];
+  for (const tagElement of $sessionPage("ul.tags > li")) {
+    tags.push($sessionPage(tagElement).text());
+  }
+
+  $sessionPage(".session_description .tags").remove();
+  const description = $sessionPage(".session_description").text();
+
+  sessionDetails.push({"url": sessionUrl, title, description, tags});
 }
 
 // Saves updated sessions list

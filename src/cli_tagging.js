fd2c6110c3ad80d8aed050b88a4a9c7b6cc5b2e4
Themi Megas <Tcm4760@gmail.com>
2026-03-24 23:45:50 -0500

    Added cli tool, categorized a couple of sessions

---
+++ src/cli_tagging.js
@@ -0,0 +1,53 @@
+import * as readline from 'node:readline';
+import fs from 'node:fs';
+
+const dataFile = "./src/pages/sessions.json";
+const lists = [
+  {"key": "1", "list": "ai"},
+  {"key": "2", "list": "criticism"},
+  {"key": "3", "list": "none"}
+];
+
+const readFile = fs.readFileSync(dataFile);
+const sessionDetails = JSON.parse(readFile).sessions;
+
+const rl = readline.createInterface({input: process.stdin, output: process.stdout});
+
+// https://stackoverflow.com/a/51506718/5813879
+const wrap = (s, w) => s.replace(
+    new RegExp(`(?![^\\n]{1,${w}}$)([^\\n]{1,${w}})\\s`, 'g'), '$1\n'
+);
+
+function askQuestion(query) {
+  return new Promise(resolve => {
+    rl.question(query, resolve);
+  });
+}
+
+for (const sessionDetail of sessionDetails) {
+  if ("list" in sessionDetail)
+    continue;
+
+  console.log("\n\n\n\n\n\n");
+  console.log("\x1b[34m", sessionDetail["title"]);
+  console.log("\x1b[31m", `Tags: ${sessionDetail["tags"].join(", ")}`);
+  console.log("\x1b[0m", wrap(sessionDetail["description"], 120).trim());
+  for (const list of lists) {
+    console.log("\x1b[1m", "\x1b[31m", `${list['key']}: `, "\x1b[0m", list["list"]);
+  }
+
+  while (true) {
+    const response = await askQuestion("Choose a list from above> ");
+    const list = lists.find(element => element["key"] === response);
+    if (list === undefined) {
+      console.log("invalid input");
+      continue;
+    }
+    sessionDetail["list"] = list['list'];
+    break;
+  }
+  fs.writeFileSync(dataFile, JSON.stringify({ sessions: sessionDetails }, null, 4));
+}
+
+console.log(sessionDetails);
+rl.close();

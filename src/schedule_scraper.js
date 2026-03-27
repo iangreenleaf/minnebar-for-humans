f0d61be4ae12cd6518f28b3ee59d09b5e677d16a
Themi Megas <Tcm4760@gmail.com>
2026-03-27 01:53:27 -0500

    Added scraping of sessions from schedule

--- src/schedule_scraper.js
+++ src/schedule_scraper.js
@@ -4,6 +4,7 @@ import fs from 'node:fs';
 import {testHTMLData} from './schedule_test_data.js';
 
 const URL = "https://sessions.minnestar.org";
+const sessionUrlBase = "https://sessions.minnestar.org/sessions/";
 
 // Uncomment this when scraping the real live schedule
 // const scheduleReq = await axios.get(URL);
@@ -17,6 +18,16 @@ const timeslots = [];
 for (const timeslotElement of timeslotElements) {
   const timeslotTime = $(timeslotElement).find("h2 > .time").text();
   const timeslotTitle = $(timeslotElement).find("h2 > .title").text();
-  timeslots.push({"time": timeslotTime, "title": timeslotTitle, "sessions": []});
+
+  const sessionElements = $(timeslotElement).find(".session").toArray();
+  const sessions = [];
+  for (const sessionElement of sessionElements) {
+    const dataElement = $(sessionElement).find("[data-session-id]")[0];
+    const sessionID = dataElement.attribs['data-session-id'];
+    const sessionURL = sessionUrlBase + sessionID;
+    sessions.push(sessionURL);
+  }
+
+  timeslots.push({"time": timeslotTime, "title": timeslotTitle, "sessions": sessions});
 }
 console.log(timeslots);

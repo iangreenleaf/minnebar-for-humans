940d61716e0bae20abe5fde1f26527f223b2ea1a
Themi Megas <Tcm4760@gmail.com>
2026-04-19 16:27:40 -0500

    Added basic stats compile script

---
+++ src/compile_stats.js
@@ -0,0 +1,48 @@
+import fs from 'node:fs';
+
+const dataFile = "./src/_data/sessions.json";
+const categoriesFile = "./src/_data/categories.json";
+const outFile = "./src/_data/stats.json";
+
+const readFile = fs.readFileSync(dataFile);
+const sessionDetails = JSON.parse(readFile);
+const categoriesReadFile = fs.readFileSync(categoriesFile);
+const categories = JSON.parse(categoriesReadFile);
+
+const categoryCounter = {}
+for (const category of categories) {
+  const categoryId = category["id"];
+  categoryCounter[categoryId] = {"aiSessions": 0, "nonAiSessions": 0, "aiVotes": 0, "nonAiVotes": 0};
+}
+const overallCounter = {"aiSessions": 0, "nonAiSessions": 0, "aiVotes": 0, "nonAiVotes": 0};
+
+for (const session of sessionDetails) {
+  if (session["list"] === "ai") {
+    overallCounter["aiSessions"] += 1;
+    overallCounter["aiVotes"] += session["participants"].length;
+    for (const category of session["categories"]) {
+      categoryCounter[category]["aiSessions"] += 1;
+      categoryCounter[category]["aiVotes"] += session["participants"].length;
+    }
+  } else if (session["list"] === "none") {
+    overallCounter["nonAiSessions"] += 1;
+    overallCounter["nonAiVotes"] += session["participants"].length;
+    for (const category of session["categories"]) {
+      categoryCounter[category]["nonAiSessions"] += 1;
+      categoryCounter[category]["nonAiVotes"] += session["participants"].length;
+    }
+  } else {
+    throw new Error(`${session['url']} was not in a valid list`);
+  }
+}
+
+overallCounter["avgAi"] = overallCounter["aiVotes"] / overallCounter["aiSessions"];
+overallCounter["avgNonAi"] = overallCounter["nonAiVotes"] / overallCounter["nonAiSessions"];
+for (const [id ,category] of Object.entries(categoryCounter)) {
+  category["avgAi"] = category["aiVotes"] / category["aiSessions"];
+  category["avgNonAi"] = category["nonAiVotes"] / category["nonAiSessions"];
+}
+
+const outData = {"overall": overallCounter, "byCategory": categoryCounter};
+
+fs.writeFileSync(outFile, JSON.stringify(outData, null, 4));

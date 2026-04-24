a015bdb94f3ca6b04f4d6c4b4d603a7206717390
Themi Megas <Tcm4760@gmail.com>
2026-04-24 17:39:22 -0500

    cleaned up unneeded code

--- src/assets/scripts/bundle/force-chart.js
+++ src/assets/scripts/bundle/force-chart.js
@@ -5,8 +5,8 @@ import sessions from "../../../_data/sessions.json";
 window.addEventListener("load", (e) => {
   const svg = d3.select("#forces");
   svg.style("background", "red");
-  const width = svg.attr("width");
-  const height = svg.attr("height");
+  const width = 960;
+  const height = 600;
 
   const simulation = d3
     .forceSimulation(sessions)
@@ -20,18 +20,6 @@ window.addEventListener("load", (e) => {
     .force("charge", d3.forceManyBody().strength(-30))
     .force("center", d3.forceCenter(width / 2, height / 2))
     .on("tick", ticked);
-
-  // const link = svg
-  //   .append("g")
-  //   .attr("class", "links")
-  //   .selectAll("line")
-  //   .data(stats["connections"])
-  //   .enter()
-  //   .append("line")
-  //   .attr("stroke-width", (d) => {
-  //     return 0.5;
-  //   })
-  //   .style("stroke", "pink");
 
   const node = svg
     .append("g")
@@ -47,20 +35,6 @@ window.addEventListener("load", (e) => {
     .attr("stroke", "yellow");
 
   function ticked() {
-    // link
-    //   .attr("x1", (d) => {
-    //     return d.source.x;
-    //   })
-    //   .attr("y1", (d) => {
-    //     return d.source.y;
-    //   })
-    //   .attr("x2", (d) => {
-    //     return d.target.x;
-    //   })
-    //   .attr("y2", (d) => {
-    //     return d.target.y;
-    //   });
-
     node
       .attr("cx", (d) => {
         return d.x;
